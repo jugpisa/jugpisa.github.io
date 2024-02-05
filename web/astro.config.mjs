c3381341c86f7b35fa4d1843df31a34e1938e462
Gabriele Zigurella <56323265+gzigurella@users.noreply.github.com>
2024-02-05 20:08:11 +0100

    Add SEO with sitemap

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,12 +2,29 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jugpisa.github.io',
   integrations: [react({
     experimentalReactChildren: true
-  }), tailwind(),
+  }), 
+  sitemap({
+    serialize(item){
+      if(item.url.includes('eventi/') || item.url.includes('archivio/')){
+        if(item.url.endsWith('eventi/') || item.url.endsWith('archivio/')) return item;
+        return undefined;
+      }
+      return item;
+    },
+    i18n: {
+      defaultLocale: 'it',
+      locales: {
+        it: 'it-IT'
+      }
+    }
+  }),
+  tailwind(),
   mdx()]
 });
--- web/src/components/Sponsor.jsx
+++ web/src/components/Sponsor.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 const getGridSize = (numberOfElements) => {
     if(numberOfElements > 2 && numberOfElements % 4 == 0) return 4;
